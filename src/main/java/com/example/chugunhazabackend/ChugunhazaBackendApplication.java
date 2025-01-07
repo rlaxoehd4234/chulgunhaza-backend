1ef83aaaffc7f61c2a8738f15bc6a3422dee03bb
saul <dlathf3210@naver.com>
2025-01-07 09:34:45 +0900

    [START]

---
+++ src/main/java/com/example/chugunhazabackend/ChugunhazaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.chugunhazabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChugunhazaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChugunhazaBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/chugunhazabackend/ChugunhazaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chugunhazabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChugunhazaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
