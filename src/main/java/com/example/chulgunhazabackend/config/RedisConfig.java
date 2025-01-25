f608ab29d94cbb895fc5140aad253327e30f8b6b
saul <dlathf3210@naver.com>
2025-01-25 17:04:53 +0900

    [FEAT] 레디스 설정 추가

--- src/main/java/com/example/chulgunhazabackend/config/RedisConfig.java
+++ src/main/java/com/example/chulgunhazabackend/config/RedisConfig.java
@@ -1,4 +1,52 @@
 package com.example.chulgunhazabackend.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.session.data.redis.RedisSessionRepository;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+import java.time.Duration;
+
+@Configuration
+@EnableRedisHttpSession
 public class RedisConfig {
+
+    @Value("${spring.data.redis.host}")
+    private String host;
+
+    @Value("${spring.data.redis.port}")
+    private int port;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory(){
+        return new LettuceConnectionFactory(host, port);
+    }
+
+    @Bean
+    @Primary
+    public RedisTemplate<String, Object> redisTemplate(){
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        redisTemplate.setEnableTransactionSupport(true);
+        return redisTemplate;
+    }
+
+    @Bean
+    public RedisSessionRepository redisSessionRepository(RedisTemplate<String, Object> redisTemplate){
+        RedisSessionRepository sessionRepository = new RedisSessionRepository(redisTemplate);
+        sessionRepository.setDefaultMaxInactiveInterval(Duration.ofSeconds(1800));
+        return sessionRepository;
+    }
+
+
+
 }
